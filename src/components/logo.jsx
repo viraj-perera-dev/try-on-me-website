a91f8aa8d74e7a59ee802cc16269834edad7ec9f
Viraj Perera <viraj.perera.dev@gmail.com>
2025-09-08 12:01:24 +0200

    start project

---
+++ src/components/logo.jsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { clsx } from 'clsx'
+import { useRef } from 'react'
+import Lottie from 'lottie-react'
+import animationData from '../../public/lottie/logo.json'
+// Import your JSON file - adjust the path as needed
+// import hangerAnimation from './path-to-your-lottie-file.json'
+
+export function Logo({ className }) {
+  const lottieRef = useRef()
+
+  const handleMouseEnter = () => {
+    if (lottieRef.current) {
+      // Stop current animation and play from beginning
+      lottieRef.current.stop()
+      lottieRef.current.play()
+    }
+  }
+
+  const handleMouseLeave = () => {
+    // Optional: You can stop the animation on mouse leave
+    // lottieRef.current?.stop()
+  }
+
+  return (
+    <div 
+      className={clsx(className, 'cursor-pointer')}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={animationData}
+        loop={false}
+        autoplay={true}
+        style={{ width: 120, height: 100 }}
+      />
+    </div>
+  )
+}
+
+export function Mark({ className }) {
+  const lottieRef = useRef()
+
+  const handleMouseEnter = () => {
+    if (lottieRef.current) {
+      lottieRef.current.stop()
+      lottieRef.current.play()
+    }
+  }
+
+  return (
+    <div 
+      className={clsx(className, 'cursor-pointer')}
+      onMouseEnter={handleMouseEnter}
+    >
+      <Lottie
+        lottieRef={lottieRef}
+        animationData={animationData}
+        loop={false}
+        autoplay={true}
+        style={{ width: 50, height: 40 }}
+      />
+    </div>
+  )
+}
